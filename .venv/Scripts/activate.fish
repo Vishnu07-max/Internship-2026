adf3e0a6dbab044bdb90492175372a39b0dea23a
Vishnu yadav <yramvishnu@gmail.com>
2026-07-07 10:22:18 +0530

    Wine

--- .venv/Scripts/activate.fish
+++ .venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'c:\Users\yramv\OneDrive\Pictures\Desktop\internship 2026\.venv'
+set -gx VIRTUAL_ENV 'D:\internship 2026\.venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
